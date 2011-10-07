82a3f58115913c4df9474ec6fce36fb7e009c7a9
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-10-07 16:52:44 +0100

    Createdb and user script updated

--- cookbooks/database/recipes/default.rb
+++ cookbooks/database/recipes/default.rb
@@ -2,10 +2,22 @@
 dbuser = node[:database][:dbuser]
 dbname = node[:database][:dbname]
 
-bash "create_db" do
+bash "createuser" do
 	user "postgres"
 	code <<-EOH
 		createuser --createdb --no-superuser --no-createrole #{dbuser}
+	EOH
+	not_if <<-EOH
+		su postgres -c "psql -c \\"SELECT * FROM pg_user WHERE usename='#{dbuser}'\\" | grep -c #{dbuser}"
+	EOH
+end
+
+bash "createdb" do
+	user "postgres"
+	code <<-EOH
 		createdb -T template_postgis --owner #{dbuser} #{dbname}
+	EOH
+	not_if <<-EOH
+		su postgres -c "psql -c \\"SELECT * FROM pg_database WHERE datname='#{dbname}'\\" | grep -c #{dbname}"
 	EOH
 end
