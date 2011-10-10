35ed7b39d3b64ef6376fdd4fee7caab25bca5634
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-10-10 10:41:28 +0100

    Updated postgres cookbook

--- cookbooks/database/recipes/default.rb
+++ cookbooks/database/recipes/default.rb
@@ -1,23 +1,15 @@
-
 dbuser = node[:database][:dbuser]
 dbname = node[:database][:dbname]
+dbpassword = node[:database][:dbname]
 
-bash "createuser" do
-	user "postgres"
-	code <<-EOH
-		createuser --createdb --no-superuser --no-createrole #{dbuser}
-	EOH
-	not_if <<-EOH
-		su postgres -c "psql -c \\"SELECT * FROM pg_user WHERE usename='#{dbuser}'\\" | grep -c #{dbuser}"
-	EOH
+postgresql_user dbuser do
+  password dbpassword
+  privileges :superuser => false, :createdb => false, :inherit => true, :login => true
 end
 
-bash "createdb" do
-	user "postgres"
-	code <<-EOH
-		createdb -T template_postgis --owner #{dbuser} #{dbname}
-	EOH
-	not_if <<-EOH
-		su postgres -c "psql -c \\"SELECT * FROM pg_database WHERE datname='#{dbname}'\\" | grep -c #{dbname}"
-	EOH
+postgresql_database dbname do
+  owner dbuser
+  template "template_postgis"
+  flags :datconnlimit => 5
+  modules [ "postgis" ]
 end
